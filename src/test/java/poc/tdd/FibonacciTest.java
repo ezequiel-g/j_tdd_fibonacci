a2fd76589dda9badcb91fbb0009d3f20b2df19c7
Ezequiel Ghione <ezequielghione@gmail.com>
2023-06-02 15:21:23 -0300

    add src

---
+++ src/test/java/poc/tdd/FibonacciTest.java
@@ -0,0 +1,14 @@
+package poc.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FibonacciTest {
+
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
+
+}
