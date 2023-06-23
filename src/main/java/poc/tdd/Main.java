571f27adee662e7032b90fbff45d45632eea239d
Ezequiel.Ghione <ezequiel.ghione@endava.com>
2023-06-23 16:24:56 -0300

    adding validate method, tests and TODO comments

--- src/main/java/poc/tdd/Main.java
+++ src/main/java/poc/tdd/Main.java
@@ -1,15 +1,46 @@
 package poc.tdd;
 
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private final Fibonacci fibonacci = new RecursiveImpl();
+    private static final Pattern DIGITS_PATTERN = Pattern.compile("^\\d+$");
+    private static final String MSG_MISSING_ARGUMENTS = "missing arguments";
+    private static final String MSG_SINGLE_ARGUMENT_SHOULD_BE_AN_INTEGER = "for single option it should be an integer";
+    private static final String MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS = "for two arguments should be numbers";
+    private final Fibonacci fibonacci = new LoopImpl();
 
     public static void main(final String[] args) {
+        final Main main = new Main();
+        main.validate(args);
         // TODO: process input using RecursiveImpl by default, printing values in console depending on values
         // TODO: process input choosing implementation
+    }
+
+    private boolean isNotANumber(final String arg) {
+        return !DIGITS_PATTERN.matcher(arg).matches();
+    }
+
+    void validate(final String[] args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException(MSG_MISSING_ARGUMENTS);
+        }
+
+        if (args.length == 1 && isNotANumber(args[0])) {
+            throw new IllegalArgumentException(MSG_SINGLE_ARGUMENT_SHOULD_BE_AN_INTEGER);
+        }
+
+        if (args.length == 2) {
+            if (isNotANumber(args[0])) {
+                throw new IllegalArgumentException(MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS);
+            }
+
+            if (isNotANumber(args[1])) {
+                throw new IllegalArgumentException(MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS);
+            }
+        }
     }
 
     public List<Integer> get(final int position) {
--- src/test/java/poc/tdd/MainTest.java
+++ src/test/java/poc/tdd/MainTest.java
@@ -1,18 +1,40 @@
 package poc.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainTest {
 
+    private static final String MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS = "for two arguments should be numbers";
+    private static final String MSG_SINGLE_ARGUMENT_SHOULD_BE_AN_INTEGER = "for single option it should be an integer";
+    private static final String MSG_MISSING_ARGUMENTS = "missing arguments";
+
     private final Main main = new Main();
 
-    @Test
-    void test() {
-        Main.main(new String[0]);
+    private static Stream<Arguments> argumentsInvalid() {
+        return Stream.of(
+                // TODO: add "all" case
+                Arguments.of(new String[]{"4", "b"}, IllegalArgumentException.class, MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS),
+                Arguments.of(new String[]{"a", "b"}, IllegalArgumentException.class, MSG_BOTH_ARGUMENTS_SHOULD_BE_INTEGERS),
+                Arguments.of(new String[]{"invalidOption"}, IllegalArgumentException.class, MSG_SINGLE_ARGUMENT_SHOULD_BE_AN_INTEGER),
+                Arguments.of(null, IllegalArgumentException.class, MSG_MISSING_ARGUMENTS),
+                Arguments.of(new String[0], IllegalArgumentException.class, MSG_MISSING_ARGUMENTS)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsInvalid")
+    void testInvalid(String[] input, Class<IllegalArgumentException> expectedType, String errorMsg) {
+        final IllegalArgumentException illegalArgumentException = assertThrows(expectedType, () -> Main.main(input));
+        assertEquals(errorMsg, illegalArgumentException.getMessage());
     }
 
     @Test
